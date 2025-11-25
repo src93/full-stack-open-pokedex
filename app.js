3d0bc629ce70dbb3a3593e148ea5a00707b7a1e3
SERGIO CUEVAS ROMERO <sergiocuevas.romero.tech@bbva.com>
2025-11-25 19:28:36 +0100

    part11 - 11.11

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 
 // get the port from env variable
-const PORT = process.env.PORT || 5050
+const PORT = process.env.PORT || 3000
 
 app.use(express.static('dist'))
 
-app.listen(PORT)
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`)
+})
